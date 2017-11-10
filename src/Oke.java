daf244ae72d03e1fa63778fa9fd7aa666da90357
Thomas Bechler <thmas.bechler@udo.edugit config --global user.email tohmas.bechler@udo.edu>
2017-11-10 09:06:45 +0100

    lernen

--- src/Oke.java
+++ src/Oke.java
@@ -145,7 +145,139 @@ public static String digitSumToString(int a){
 }
 
 
+    public static int sumUpNegatives(int[] pArray){
+        int sum = 0;
+        for(int i = 0; i <= pArray.length -1; i++){
+            if(pArray[i] < 0){
+                sum = sum + pArray[i];
+            }
+        }
+        return sum;
+    }
+    public static int maximum(int[] pArray){
+        int biggy = pArray[0];
+        for(int i = 1; i <= pArray.length -1; i++){
+            if(pArray[i]>biggy){
+                biggy = pArray[i];
+            }
+        }
+        return biggy;
+    }
+    public static int countMaximum(int[] pArray){
+        int sumOfBiggies = 0;
+        int biggy = maximum(pArray);
 
+        for(int i = 0; i<= pArray.length -1; i++){
+            if(biggy == pArray[i]){
+                sumOfBiggies++;
+            }
+        }
+        return sumOfBiggies;
+    }
+    public static boolean sorted(int[] cavid){
+        int x = cavid[0];
+        for(int i = 1; i <= cavid.length -1; i++){
+            if(x<=cavid[i]){
+                x = cavid[i];
+
+            }else return false;
+        }
+        return true;
+    }
+    public static boolean checkArray(int[] cavid){
+        int x = cavid.length -1;
+        for(int i = 0; i <= cavid.length -1;i++){
+            if(cavid[i] == cavid[x]){
+                x--;
+            }else return false;
+        }
+        return true;
+    }
+    public static int[] increaseArray(int[] cavid,int x){
+        for(int i = 0; i < cavid.length; i++){
+            cavid[i] = cavid[i] + x;
+        }
+        return cavid;
+    }
+    public static int[] doubleIfContainsPositive(int[] cavid){
+        for(int i = 0; i < cavid.length; i++){
+            if(cavid[i] > 0){
+                for(int x = 0;x < cavid.length; x++){
+                    cavid[x] = cavid[x] + cavid[x];
+                }
+                break;
+            }
+        }
+        return cavid;
+    }
+    public static String toString(int[] cavid){
+        String zahlen = "";
+        for(int i = 0; i < cavid.length; i++ ){
+            if(i == 0){
+                zahlen = "" + cavid[i];
+            }else
+                zahlen = zahlen + "," + cavid[i];
+        }
+        return zahlen;
+    }
+    public static int[] copyStartingValues(int[] pArray){
+        int[] nArray = new int[3];
+        if(pArray.length < 3){
+            nArray = pArray;
+        }else{
+            for(int i = 0; i <= 2; i++){
+                nArray[i] = pArray[i];
+            }
+        }
+        return nArray;
+    }
+    public static int countNegatives(int[] pArray){
+        int x = 0;
+        for(int i = 0; i < pArray.length; i++){
+            if(pArray[i] < 0){
+                x++;
+            }
+        }
+        return x;
+    }
+    public static int[] selectNegatives(int[] pArray){
+        int[] nArray = new int[countNegatives(pArray)];
+        int x = 0;
+        for(int i = 0; i < pArray.length; i++){
+            if(pArray[i] < 0){
+                nArray[x] = pArray[i];
+                x++;
+            }
+        }
+        return nArray;
+    }
+    public static boolean twoTimes(int [] arr){
+        if(arr.length < 2){
+            return false;
+        }else{
+            for(int i = 0; i < arr.length; i++){
+                int y = 0;
+                for(int j = 0; j < arr.length; j++){
+                    if(arr[i] == arr[j]){
+                        y++;
+                    }
+                }
+                if(y != 2){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static int[] addArrays(int[] arr, int[] ay){
+        int[] cavid = new int[arr.length];
+        if(arr.length == ay.length){
+            for(int i = 0; i < arr.length; i++){
+                cavid[i] = arr[i] + ay[i];
+            }
+        }else{cavid = new int[0];}
+        return cavid;
+    }
 
 
 
